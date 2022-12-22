0a22ccc96b74ba20bfb999317c3022bc5feba3c3
Ali Mohammadpour <aliemohammadpour@gmail.com>
2022-12-22 20:22:33 +0330

    Add Main Args Sample

---
+++ samples/main_args/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{   
+    // get two args and calculate the sum, mult with operator
+    //
+    // ./main 1 2 +
+    // ./main 1 2 -
+    // ./main 1 2 *
+    // ./main 1 2 /
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    char op = argv[3][0];
+
+    switch (op)
+    {
+    case '+':
+        cout << a + b << endl;
+        break;
+    case '-':
+        cout << a - b << endl;
+        break;
+    case 'x':
+        cout << a * b << endl;
+        break;
+    default: break;
+    }
+
+    // cout << "argc: " << argc << endl;
+    // cout << "argv[0]: " << argv[0] << endl;
+    // if (argc > 1) {
+
+    //     cout << "Hello, " << argv[1] << endl;
+    // } else {
+    //     cout << "Hello, World!" << endl;
+    // }
+    
+    return 0;
+}
