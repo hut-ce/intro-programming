ce081ac61981094d33063e410a2f3fc6c5a4072b
Ali Mohammadpour <aliemohammadpour@gmail.com>
2023-01-02 17:51:02 +0330

    Update Sample

--- samples/main_args/main.cpp
+++ samples/main_args/main.cpp
@@ -12,23 +12,30 @@ int main(int argc, char const *argv[])
     // ./main 1 2 *
     // ./main 1 2 /
 
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    char op = argv[3][0];
-
-    switch (op)
-    {
-    case '+':
-        cout << a + b << endl;
-        break;
-    case '-':
-        cout << a - b << endl;
-        break;
-    case 'x':
-        cout << a * b << endl;
-        break;
-    default: break;
+    if (argc < 4) {
+        
+        cout << "Usage: ./main 1 2 +" << endl;
+        return 0;
     }
+    else {
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+        char op = argv[3][0];
+        switch (op)
+            {
+            case '+':
+                cout << a + b << endl;
+                break;
+            case '-':
+                cout << a - b << endl;
+                break;
+            case 'x':
+                cout << a * b << endl;
+                break;
+            default: break;
+    }
+    }
+    
 
     // cout << "argc: " << argc << endl;
     // cout << "argv[0]: " << argv[0] << endl;
